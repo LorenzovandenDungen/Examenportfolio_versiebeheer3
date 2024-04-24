ac744efb34f43c7c8f73c44d70ac023110f909be
LorenzovandenDungen <2130766@talnet.nl>
2024-04-24 11:38:30 +0200

    Offerte

--- src/views/OfferteView.vue
+++ src/views/OfferteView.vue
@@ -1,17 +1,49 @@
 <template>
   <div>
-    <GlobalHeader />
-    <h1>Offerte Aanvragen</h1>
-    <form class="offerte-form">
-      <!-- Offerte form fields based on wireframe -->
-    </form>
+    <div class="offerte-container">
+      <div class="offerte-content">
+        <h1>Website Offerte Aanvragen</h1>
+        <form class="offerte-form" @submit.prevent="submitForm">
+          <div class="form-group">
+            <label for="naam">Naam:</label>
+            <input type="text" id="naam" v-model="naam" required>
+          </div>
+          <div class="form-group">
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" v-model="email" required>
+          </div>
+          <div class="form-group">
+            <label for="telefoon">Telefoon:</label>
+            <input type="tel" id="telefoon" v-model="telefoon" required>
+          </div>
+          <div class="form-group">
+            <label for="bericht">Bericht:</label>
+            <textarea id="bericht" v-model="bericht" required></textarea>
+          </div>
+          <button type="submit">Verzend Offerte</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import GlobalHeader from '../components/GlobalHeader.vue'
+import { ref } from 'vue'
+
+const naam = ref('');
+const email = ref('');
+const telefoon = ref('');
+const bericht = ref('');
+
+const submitForm = () => {
+  // Logica om het formulier te verzenden
+  console.log('Naam:', naam.value);
+  console.log('E-mail:', email.value);
+  console.log('Telefoon:', telefoon.value);
+  console.log('Bericht:', bericht.value);
+}
 </script>
 
 <style scoped>
-
+@import '../assets/styles.css';
 </style>
