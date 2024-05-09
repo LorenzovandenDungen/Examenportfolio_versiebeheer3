d60f88f4028fd2ef1e3254b3de8add529bdf068d
LorenzovandenDungen <2130766@talnet.nl>
2024-05-09 15:49:19 +0200

    save

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import TarievenView from '../views/TarievenView.vue'
-import OfferteView from '../views/OfferteView.vue'
-import ContactView from '../views/ContactView.vue'
-import WebsitesView from '../views/WebsitesView.vue' // Added line
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import TarievenView from '@/views/TarievenView.vue';
+import OfferteView from '@/views/OfferteView.vue';
+import ContactView from '@/views/ContactView.vue';
+import WebsitesView from '@/views/WebsitesView.vue';
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/tarieven', name: 'tarieven', component: TarievenView },
   { path: '/offerte', name: 'offerte', component: OfferteView },
   { path: '/contact', name: 'contact', component: ContactView },
-  { path: '/websites', name: 'websites', component: WebsitesView } // Added line
-  // Add other routes here if necessary
-]
+  { path: '/websites', name: 'websites', component: WebsitesView }
+  // Voeg hier andere routes toe indien nodig
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
